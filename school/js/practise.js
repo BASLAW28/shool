7d86a97e7b238f8ba31719244054fa69df4f2526
BASLAW28 <113939106+BASLAW28@users.noreply.github.com>
2022-10-11 10:06:46 +0100

    html learning

---
+++ school/js/practise.js
@@ -0,0 +1,7 @@
+// let log=prompt('Enter Password')
+// let Password='javascript'
+// log == Password ? alert('correct password') : alert('wrong password');  prompt('Comfirm Password')
+let me = document.querySelector('.e2')
+me = addEventListener('click',()=>{
+    me.classList.toggle('e3')
+})
